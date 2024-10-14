1ebc12f48cf4e142f48571f35a6fa7cafee0a481
Viktor Petruk <damnnn261@gmail.com>
2024-10-14 21:59:18 +0300

    make a BaseNavigation component, make an About page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
 
 <template>
 	<div>
-		Default Layout
+		<BaseNavigation />
 		<slot />
 	</div>
 </template>
---
+++ app/pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<main>
+		<section class="container flex flex-col py-20">
+			<h1 class="text-2xl sm:text-3xl md:text-6xl  font-extrabold  mb-6 text-balance">About Nuxtcipes:</h1>
+			<p class="lg:text-xl lg:font-extrabold text-sm sm:text-md mb-6 text-balance max-w-screen-lg">
+				Nuxtcipes is a recipe application based on the dummyJSON API, created by Viktor Petruk for
+				the <span class="font-bold">Build Modern Apps with Nuxt</span> Test.
+			</p>
+		</section>
+	</main>
+</template>
